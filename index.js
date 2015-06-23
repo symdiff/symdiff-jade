75caea2951f66ca2605ee62879d9923e503a03a7
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-06-23 20:05:13 +0200

    #1 use jade lexer and parser, bump

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-var jade = require('jade'),
-    symdiffHTML = require('symdiff-html');
+var lex = require('jade-lexer'),
+    parse = require('jade-parser');
+
+function walk(node, fn) {
+    fn(node);
+    if (node.nodes && node.nodes.forEach) {
+        node.nodes.forEach(function(child) {
+            walk(child, fn);
+        });
+    }
+}
+
+function dedup(c, i, array) {
+    return array.lastIndexOf(c) === i;
+}
 
 function symdiffJade(jadeString) {
-    return symdiffHTML(jade.render(jadeString));
+    var ast = parse(lex(jadeString)),
+        classes = [];
+
+    walk(ast, function(node) {
+        if (!node.attrs) {
+            return;
+        }
+        
+        node.attrs.forEach(function(attr) {
+            if (attr.name !== 'class') {
+                return;
+            }
+            var value = attr.val;
+            if (!attr.escaped) {
+                // escape
+                value = value.replace(/\'/gi, '');
+            }
+            classes.push(value);
+        });
+    });
+
+    return classes.filter(dedup);
 }
 
 module.exports = symdiffJade;
