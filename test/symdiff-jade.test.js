008ade9ce4c55f66688999bd81c129a0e344b26e
Bailey Parker <b@ileyparker.com>
2015-08-09 18:44:49 -0400

    Updates test fixtures to use real indented jade

--- test/symdiff-jade.test.js
+++ test/symdiff-jade.test.js
@@ -1,4 +1,19 @@
-var extract = require('../index');
+var extract  = require('../index'),
+    readFile = require('fs').readFile,
+    join     = require('path').join,
+    resolve  = require('path').resolve;
+
+function readFixture(filename, cb) {
+    var path = resolve(join(__dirname, filename));
+
+    readFile(path, function(err, data) {
+        if(err) {
+            expect.fail(null, null, err.toString());
+        }
+
+        cb(data.toString());
+    });
+}
 
 describe('symdiff-jade', function() {
     it('should work with empty jade', function() {
@@ -6,33 +21,43 @@ describe('symdiff-jade', function() {
         expect(result.length).to.equal(0);
     });
 
-    it('should extract a class', function() {
-        var testJade = 'div.grid',
-            result = extract(testJade);
+    it('should extract a class', function(done) {
+        readFixture('fixtures/single_class.jade', function(testJade) {
+            var result = extract(testJade);
 
-        expect(result.length).to.equal(1);
-        expect(result[0]).to.equal('grid');
+            expect(result.length).to.equal(1);
+            expect(result[0]).to.equal('grid');
+            done();
+        });
     });
 
-    it('should extract multiple classes', function() {
-        var testJade = 'div.grid.grid-col',
-            result = extract(testJade);
+    it('should extract multiple classes', function(done) {
+        readFixture('fixtures/multiple_classes.jade', function(testJade) {
+            var result = extract(testJade);
 
-        expect(result.length).to.equal(2);
-        expect(result[0]).to.equal('grid');
-        expect(result[1]).to.equal('grid-col');
+            expect(result.length).to.equal(2);
+            expect(result[0]).to.equal('grid');
+            expect(result[1]).to.equal('grid-col');
+            done();
+        });
     });
 
-    it('should extract nothing when there are no classes', function() {
-        var testJade = 'script(type="text/javascript")',
-            result = extract(testJade);
+    it('should extract nothing when there are no classes', function(done) {
+        readFixture('fixtures/no_classes.jade', function(testJade) {
+            var result = extract(testJade);
 
-        expect(result.length).to.equal(0);
+            expect(result.length).to.equal(0);
+            done();
+        });
     });
 
-    it('should not contain duplicates', function() {
-        var testJade = 'div.grid.grid',
-            result = extract(testJade);
-        expect(result.length).to.equal(1);
+    it('should not contain duplicates', function(done) {
+        readFixture('fixtures/duplicate_classes.jade', function(testJade) {
+            var result = extract(testJade);
+
+            expect(result.length).to.equal(1);
+            expect(result[0]).to.equal('grid-col');
+            done();
+        });
     });
 });
